d5d1116c6630c752d8826f07bb2b1e9aad294bfc
Blackie0808 <xiiaoolun0813@gmail.com>
2022-02-14 15:01:14 +0800

    Practical 3.3 Start an external activity

---
+++ app/src/main/java/com/example/intentdemo/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.intentdemo
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnA: Button = findViewById(R.id.btnA)
+
+        btnA.setOnClickListener(){
+            val intentA: Intent = Intent(this, AboutActivity::class.java)
+
+            intentA.putExtra("StudentName","John")
+
+            startActivity(intentA)
+        }
+
+        val btnB:Button = findViewById(R.id.btnA)
+        btnB.setOnClickListener(){
+
+            //val telNo = Uri.parse("tel:0123456789")
+            //val intentB: Intent = Intent(Intent.ACTION_DIAL, telNo)
+
+            val geo = Uri.parse("geo:3.140853,101.693207")
+            val intentB:Intent = Intent(Intent.ACTION_VIEW, geo)
+
+            startActivity(intentB)
+        }
+
+
+
+    }
+}
